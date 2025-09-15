452f6cc44343f061bcbddb0f79ee28cdb750dec8
poledna0 <henriquepoledna@proton.me>
2025-09-15 19:45:38 -0300

    comeco hashtable

--- HashTable/src/main.rs
+++ HashTable/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::option::Option;
+
+struct No{
+    valor: String,
+    proximo: Option<u8>,
+}
+impl No{
+    fn new(valor: String, proximo: Option<u8>)-> Self{
+        No { valor: (valor), proximo: (proximo) }
+    }
+    
+}
+
+struct Lista {
+    nos: Vec<No>,
+    inicio: Option<usize>,
+    fim: Option<usize>,
+}
+
+
+impl Lista {
+    fn new() -> Self {
+        Lista {
+            nos: Vec::new(),
+            inicio: None,
+            fim: None,
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+fn main(){
+
 }
